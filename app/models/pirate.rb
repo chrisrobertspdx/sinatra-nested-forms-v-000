f8a8c1cba68d122825e360ca2cfee11b832ecf59
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-21 17:32:31 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name,:weight,:height
+  @@all = []
+  def initialize(attributes={})
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear
+    @@all.clear
+  end
 end
